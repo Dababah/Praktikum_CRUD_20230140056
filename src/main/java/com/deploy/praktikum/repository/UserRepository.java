473ba72837fc93d40c11714fbbe9791f90a06061
Your Name <your.email@example.com>
2026-03-02 10:59:16 +0700

    bikin repository buat user

---
+++ src/main/java/com/deploy/praktikum/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.deploy.praktikum.repository;
+
+import com.deploy.praktikum.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
